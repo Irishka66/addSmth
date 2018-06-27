5497e8dd6503d907754859a40ee3e7c587569c6b
Irina Stanishevskaya <IrinaStanishevskaya@coherentsolutions.com>
2018-06-27 11:05:45 +0300

    added modal-sure on delete friend-info

--- src/app/adding/friends/friends.component.ts
+++ src/app/adding/friends/friends.component.ts
@@ -15,16 +15,28 @@ import {
   templateUrl: './friends.component.html',
   styleUrls: ['./friends.component.scss'],
   animations: [
-    trigger('friendState', [
+    trigger('leave', [
       state('active',   
       style({
       })),
       transition('* => void', [
-        animate('800ms ease-in', style({
+        animate('400ms ease-in', style({
           transform: 'scale(0.5)',
           opacity: 0
         }))
-      ])
+      ]),
+    ]),
+    trigger('enter', [
+      state('active',   
+      style({
+      })),
+      transition('void => *', [
+        style({
+          transform: 'scale(0.5)',
+          opacity: 0
+        }),
+        animate('400ms ease-in')
+      ]),
     ]),
     
   ]
@@ -33,12 +45,17 @@ import {
 export class FriendsComponent implements OnInit {
 
   nameList: Array<any>;
+  existModal: boolean;
+  iFromOpenModal: number;
+
   constructor(private router: Router, private friendService: FriendService) { }
 
   ngOnInit() {
     if (JSON.parse(localStorage.getItem('friends')) !== null) {
       this.friendService.friendList = JSON.parse(localStorage.getItem('friends'));
     }
+
+    this.iFromOpenModal = -1;
   }
 
   editFriend(i) {
@@ -50,4 +67,15 @@ export class FriendsComponent implements OnInit {
     this.friendService.saveLocalFriends();
   }
 
+  openModal(event, i) {
+    event.stopPropagation();
+    this.iFromOpenModal = i;
+    this.existModal = true
+  }
+
+  closeModal() {
+    event.stopPropagation();
+    this.iFromOpenModal = -1;
+    this.existModal = false;
+  }
 }
