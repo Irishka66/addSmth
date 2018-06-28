1e69d4bdd8367070e05d640dbd7184a50125cf19
Irina Stanishevskaya <IrinaStanishevskaya@coherentsolutions.com>
2018-06-28 18:20:00 +0300

    i did it! made reactive chart

--- src/app/adding/statistics/chartjs/chartjs.component.ts
+++ src/app/adding/statistics/chartjs/chartjs.component.ts
@@ -15,8 +15,12 @@ export class ChartjsComponent implements OnInit {
       this.chartData = [
         { data: arr[0], label: 'Click 1' },
       ];
-      this.chartLabels = arr[1];
-      console.log(arr[1]);
+
+      setTimeout(() => {
+        this.chartLabels = arr[1];
+      }, 50);
+      // this.chartLabels = arr[1];
+      // console.log(arr[1]);
       // this.chartLabels = ['Thu Mar 01 2001 00:00:00 GMT+0300 (Belarus Standard Time)', 'Fri Mar 02 2001 00:00:00 GMT+0300 (Belarus Standard Time)', 'Sat Mar 03 2001 00:00:00 GMT+0300 (Belarus Standard Time)', 'Sun Mar 04 2001 00:00:00 GMT+0300 (Belarus Standard Time)', 'Mon Mar 05 2001 00:00:00 GMT+0300 (Belarus Standard Time)', 'Tue Mar 06 2001 00:00:00 GMT+0300 (Belarus Standard Time)', 'Wed Mar 07 2001 00:00:00 GMT+0300 (Belarus Standard Time)', 'Thu Mar 08 2001 00:00:00 GMT+0300 (Belarus Standard Time)', 'Fri Mar 09 2001 00:00:00 GMT+0300 (Belarus Standard Time)', 'Sat Mar 10 2001 00:00:00 GMT+0300 (Belarus Standard Time)'];
 
       // console.log(this.chartLabels);
