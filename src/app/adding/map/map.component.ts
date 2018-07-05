59204b5dd32c12bf9bb27bf0407aaeaff8689f22
Irina Stanishevskaya <IrinaStanishevskaya@coherentsolutions.com>
2018-07-05 17:09:18 +0300

    able to add picture, have problem with reloading page and picture

--- src/app/adding/map/map.component.ts
+++ src/app/adding/map/map.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MouseEvent } from '@agm/core';
+import {MapService} from '../../services/map.service';
 
 @Component({
   selector: 'app-map',
@@ -8,9 +9,13 @@ import { MouseEvent } from '@agm/core';
 })
 export class MapComponent implements OnInit {
 
-  constructor() { }
+  constructor(private mapService: MapService) { }
 
   ngOnInit() {
+    if (JSON.parse(localStorage.getItem('markers')) !== null) {
+      this.markers = JSON.parse(localStorage.getItem('markers'));
+      this.mapService.markers = this.markers;
+    }
   }
   zoom: number = 12;
   // initial center position for the map
@@ -19,79 +24,56 @@ export class MapComponent implements OnInit {
   markers: marker[] = [];
 
   clickedMarker(marker: string, index: number) {
-    console.log(marker);
-    console.log(index);
-    // console.log(`clicked the marker: ${label || index}`)
   }
   
   mapClicked($event: MouseEvent) {
+    let id: string = (Date.now().toString(36) + Math.random().toString(36).substr(2, 5)).toUpperCase(); 
     this.markers.push({
+      id: id, 
       lat: $event.coords.lat,
       lng: $event.coords.lng,
       text: 'Nice place',
       imgShow: false,
       draggable: false,
     });
+    this.mapService.markers = this.markers;
+    this.mapService.saveLocalMarkers();
   }
   
   markerDragEnd(m: marker, $event: MouseEvent) {
-    console.log('dragEnd', m, $event);
+    // console.log('dragEnd', m, $event);
   }
 
   deleteImg(i) {
     this.markers[i]['imgShow'] = false;
-    console.log(this.markers);
+    this.mapService.saveLocalMarkers();
   }
 
   editInfoText(event, i) {
-    console.log(i);
-    console.log(event);
-    console.log(event.srcElement.innerText);
     this.markers[i]['text'] = event.srcElement.innerText;
-    console.log(this.markers);
+    this.mapService.saveLocalMarkers();
   }
 
   readURL(event, i){
-    console.log(event);
-    // this.markers[i]['imgShow'] = true;
-    // var getImagePath = URL.createObjectURL(event.target.files[0]);
-    // this.markers[i]['imgPath'] = getImagePath;
-    // console.log(getImagePath);
-    console.log(i);
-    // console.log(this.markers);
+    this.markers[i]['imgShow'] = true;
+    var getImagePath = URL.createObjectURL(event.target.files[0]);
+    this.markers[i]['imgPath'] = getImagePath;
+    this.mapService.saveLocalMarkers();
    }
 
-  deleteDescr(i) {
+  deleteMarker(i) {
     this.markers.splice(i, 1);
+    this.mapService.saveLocalMarkers();
   }
 
-
-  
-  
-  // markers: marker[] = [
-	//   {
-	// 	  lat: 44.04607127363113,
-	// 	  lng: 12.60,
-	// 	  label: 'A',
-	// 	  draggable: true
-	//   },
-	//   {
-	// 	  lat: 44.04607127363113,
-	// 	  lng: 12.50,
-	// 	  label: 'B',
-	// 	  draggable: false
-	//   },
-	//   {
-	// 	  lat: 44.04,
-	// 	  lng: 12.605696990932529,
-	// 	  label: 'C',
-	// 	  draggable: true
-	//   }
-  // ]
+  clickInfo() {
+    console.log('777');
+  }
 }
 
 
 interface marker {
+  id: any;
 	lat: number;
 	lng: number;
   label?: string;
--- src/app/services/map.service.ts
+++ src/app/services/map.service.ts
@@ -6,4 +6,11 @@ import { Injectable } from '@angular/core';
 export class MapService {
 
   constructor() { }
+
+  public markers: Array<any> = [];
+
+  public saveLocalMarkers() {
+    let localMarkers = JSON.stringify(this.markers);
+    localStorage.setItem('markers', localMarkers);
+  }
 }
